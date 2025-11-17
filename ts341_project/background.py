60d1dd8035435ce7b5f054f5ef7495b337d7f618
athjs <mathisppr@gmail.com>
2025-11-17 11:33:46 +0100

    adding contours finding

--- ts341_project/background.py
+++ ts341_project/background.py
@@ -11,6 +11,7 @@ def removeBackground(path: str) -> cv.VideoCapture:
     cap: cv.VideoCapture = utils.openvideo(path)
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
     fgbg = cv.createBackgroundSubtractorMOG2()
+    # For the creation of the video without backgorund.
     frame_width: int = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
     frame_height: int = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
     fourcc = cv.VideoWriter_fourcc(*"mp4v")
@@ -21,10 +22,27 @@ def removeBackground(path: str) -> cv.VideoCapture:
         ret, frame = cap.read()
         if not ret:
             raise Exception("An error occured during the video")
+        # Video without background
         fgmask = fgbg.apply(frame)
+        # thresholding the image to remove some noises
+        ret, threshold = cv.threshold(fgmask.copy(), 120, 255, cv.THRESH_BINARY)
+        # removing some useless points (KNN like)
         fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+        # find countours of moveable objects
+        contours, hier = cv.findContours(
+            threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE
+        )
+
+        # check every contour if are exceed certain value draw bounding boxes
+        for contour in contours:
+            # if area exceed certain value then draw bounding boxes
+            if cv.contourArea(contour) > 20 and cv.contourArea(contour) < 2500 :
+                (x, y, w, h) = cv.boundingRect(contour)
+                cv.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        # add the frame to the video without background
         out.write(fgmask)
-        cv.imshow("Frame", fgmask)
+        # open the raw video with rectangles on moving objects
+        cv.imshow("Frame", frame)
         if cv.waitKey(30) & 0xFF == ord("q"):
             break
 
